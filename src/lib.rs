c8d3ad791a8c8eb9c5dfa01e668855205f7230c8
Duncan Fairbanks <duncanfairbanks6@gmail.com>
2023-11-05 17:53:07 -0800

    use default $source_path

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,9 @@ pub struct EmbeddedAssetPlugin;
 
 impl Plugin for EmbeddedAssetPlugin {
     fn build(&self, app: &mut App) {
-        // We get to choose some prefix relative to the workspace root which
-        // will be ignored in "embedded://" asset paths.
-        let omit_prefix = "examples/asset";
         // Path to asset must be relative to this file, because that's how
         // include_bytes! works.
-        embedded_asset!(app, omit_prefix, "bevy_pixel_light.png");
+        embedded_asset!(app, "bevy_pixel_light.png");
 
         app.add_systems(Startup, setup);
     }
