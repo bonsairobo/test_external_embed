a729c8d6622b735f41cb82e238a1649b7ec0955a
Duncan Fairbanks <duncanfairbanks6@gmail.com>
2023-11-05 17:41:41 -0800

    first commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "NixOS environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShell.${system} = with pkgs;
+      mkShell rec {
+        # Rust binaries need to be able to link and load some system libraries.
+        buildInputs = [
+          alsa-lib
+          pkg-config
+          stdenv.cc.cc.lib
+          udev
+          vulkan-loader
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+          wayland
+        ];
+        LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+      };
+  };
+}
